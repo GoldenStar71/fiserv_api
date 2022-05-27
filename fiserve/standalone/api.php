ca3f53a50b66c5c90247befbafcb388d0609c48e
Apollon-Star <apollon71star@gmail.com>
2022-05-28 03:37:33 +0900

    fix v1.0

--- fiserve/standalone/api.php
+++ fiserve/standalone/api.php
@@ -109,10 +109,10 @@ try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'shared81.accountservergroup.com';                     //Set the SMTP server to send through
+    $mail->Host       = '';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'api-smtp@rebootit.info';                     //SMTP username
-    $mail->Password   = 'IzgP+mwgT6Z[';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
